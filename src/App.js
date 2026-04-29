359bd8ecff8b1a03053c5300162734741e1249d1
Mohan Raj <einsatz.spmr@gmail.com>
2026-04-29 10:29:54 +0530

    feat: add routing with /india route in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import India from './pages/India';
 import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <nav className="nav-bar">
+        <Link to="/india" className="nav-link">India</Link>
+      </nav>
+      <div className="home-content">
+        <h1>Hello!</h1>
+        <p>Welcome to my simple React app.</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-      <p>Welcome to my simple React app.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/india" element={<India />} />
+      </Routes>
+    </Router>
   );
 }
 
