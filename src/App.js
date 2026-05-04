bfb6275289a1123b68441b28c221a119ecd4c721
mohanrajsp <mohanraj.sp@citrix.com>
2026-05-04 16:35:03 +0530

    added route /greece

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import India from './pages/India';
 import Us from './pages/Us';
+import Greece from './pages/Greece';
 import './App.css';
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <nav className="nav-bar">
         <Link to="/india" className="nav-link">India</Link>
         <Link to="/us" className="nav-link">US</Link>
+        <Link to="/greece" className="nav-link">Greece</Link>
       </nav>
       <div className="home-content">
         <h1>Hello!</h1>
@@ -26,6 +28,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/india" element={<India />} />
         <Route path="/us" element={<Us />} />
+        <Route path="/greece" element={<Greece />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Greece.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Greece() {
+  return (
+    <div className="App">
+      <h1>Welcome Greece!</h1>
+    </div>
+  );
+}
+
+export default Greece;
+
